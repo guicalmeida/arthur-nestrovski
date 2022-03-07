7cf3089228e68c94b08593931b647960930beafd
Guilherme Almeida <guica.almeida@gmail.com>
2022-03-07 18:29:26 -0300

    feat: created books page

--- src/templates/home/livros/index.tsx
+++ src/templates/home/livros/index.tsx
@@ -1,17 +1,33 @@
 import ItemCarousel from 'components/itemCarousel/itemCarousel'
 import NavDrawer from 'components/navDrawer'
 import NavHeader from 'components/navHeader'
-import { LivrosProps } from 'types/api'
+import slugify from 'slugify'
+import { LivroProps, LivrosProps } from 'types/api'
 
 const LivrosPage = ({ livros, categoriasLivro }: LivrosProps) => {
   const bc = {
     Livros: 'livros',
   }
+
   return (
     <>
       <NavDrawer />
       <NavHeader breadcrumbs={bc}>Livros</NavHeader>
-      <ItemCarousel categoriasLivro={categoriasLivro} livros={livros} />
+      {categoriasLivro.map((categoria) => {
+        const thisGenreBooks: LivroProps[] = []
+        livros.map((livro) => {
+          if (livro?.categoriaLivro?.titulo === categoria?.titulo) {
+            thisGenreBooks.push(livro)
+          }
+        })
+        return (
+          <ItemCarousel
+            key={slugify(categoria.titulo)}
+            itens={thisGenreBooks}
+            titulo={categoria.titulo}
+          />
+        )
+      })}
     </>
   )
 }
