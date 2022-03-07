39d393c8a1b67903ce72aa84578e3b25098e3e32
Guilherme Almeida <guica.almeida@gmail.com>
2022-03-07 17:45:08 -0300

    refactor: api edits

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,28 +1,37 @@
+export interface LivroProps {
+  titulo: string
+  subtitulo: string
+  linkParaCompra: string
+  isbn: string
+  ilustracoes: string
+  editora: TitleProps
+  descricao: RichTextProps
+  cidade: TitleProps
+  categoriaLivro: TitleProps
+  capa: ImageProps
+  autor: string
+  ano: AnoProps
+}
+
+export interface TitleProps {
+  titulo: string
+}
+
+export interface RichTextProps {
+  html: string
+}
+
+export interface AnoProps {
+  ano: number
+}
+
 export interface LivrosProps {
-  livros: {
-    titulo: string
-    subtitulo: string
-    linkParaCompra: string
-    isbn: string
-    ilustracoes: string
-    editora: {
-      titulo: string
-    }
-    descricao: {
-      html: string
-    }
-    cidade: {
-      titulo: string
-    }
-    categoriaLivro: {
-      titulo: string
-    }
-    capa: {
-      url: string
-    }
-    autor: string
-    ano: {
-      ano: number
-    }
-  }
+  livros: LivroProps[]
+  categoriasLivro: TitleProps[]
+}
+
+export interface ImageProps {
+  url: string
+  width: number
+  height: number
 }
