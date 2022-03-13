1050719d9de5be7efb5f8464ba6a323ffbb772ec
Guilherme Almeida <guica.almeida@gmail.com>
2022-03-13 16:43:50 -0300

    refactor: change js fdor css

--- src/components/itemCarousel.tsx
+++ src/components/itemCarousel.tsx
@@ -1,11 +1,59 @@
 /* eslint-disable @next/next/no-img-element */
-import { Box, Container, Fade, Typography } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 import { LivroProps } from 'types/api'
 import Slider from 'react-slick'
 
 import Link from 'components/link'
 import universalSlugify from 'services/slugifyHelper'
-import { CSSProperties, useState } from 'react'
+import { CSSProperties } from 'react'
+import { styled } from '@mui/system'
+
+const StyledContainer = styled(Container)`
+  margin: 15px 0;
+  height: 540px;
+  max-width: 100vw !important;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+const StyledBox = styled(Box)`
+  width: 90%;
+  max-width: 1200px;
+  margin: 0 auto;
+  text-align: center;
+`
+
+const Overlay = styled('div')`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  backdrop-filter: blur(3px);
+  background-color: #52525294;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  transition: all 0.2s ease-in-out;
+  opacity: 0;
+
+  &:hover {
+    opacity: 1;
+    transition: all 0.2s ease-in-out;
+  }
+`
+
+const ImageContainer = styled('div')`
+  position: relative;
+  height: fit-content;
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+`
 
 export default function ItemCarousel({ parentRoute, itens, titulo }: Props) {
   const settings = {
@@ -43,30 +91,9 @@ export default function ItemCarousel({ parentRoute, itens, titulo }: Props) {
     ],
   }
 
-  let initialHoverState = {} as HoverProps
-  itens.forEach((item) => {
-    const slug = universalSlugify(item.titulo)
-    initialHoverState = {
-      ...initialHoverState,
-      [slug]: false,
-    }
-  })
-  const [hover, setHover] = useState(initialHoverState)
-
   return (
     <>
-      <Container
-        sx={{
-          margin: '15px 0',
-          height: '540px',
-          maxWidth: '100vw !important',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          backgroundColor: 'background.paper',
-        }}
-      >
+      <StyledContainer sx={{ backgroundColor: 'background.paper' }}>
         <Typography
           sx={{
             letterSpacing: '5px',
@@ -79,14 +106,7 @@ export default function ItemCarousel({ parentRoute, itens, titulo }: Props) {
         >
           {titulo}
         </Typography>
-        <Box
-          sx={{
-            width: '90%',
-            maxWidth: '1200px',
-            margin: '0 auto',
-            textAlign: 'center',
-          }}
-        >
+        <StyledBox>
           <Slider {...settings}>
             {itens.map((item) => {
               let bookHoverStyle: CSSProperties = {
@@ -108,87 +128,47 @@ export default function ItemCarousel({ parentRoute, itens, titulo }: Props) {
                     style={{ width: 'fit-content', margin: 'auto' }}
                     link={parentRoute ? `/${parentRoute}/${slug}` : `/${slug}`}
                   >
-                    <div
-                      style={{
-                        position: 'relative',
-                        height: 'fit-content',
-                        width: 'fit-content',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        margin: 'auto',
-                      }}
-                      onMouseEnter={() => {
-                        setHover({
-                          ...hover,
-                          [slug]: true,
-                        })
-                      }}
-                      onMouseLeave={() => {
-                        setHover({
-                          ...hover,
-                          [slug]: false,
-                        })
-                      }}
-                    >
+                    <ImageContainer>
                       <img
                         src={item?.capa?.url}
                         alt={'capa de ' + item?.titulo}
-                        style={{ ...bookHoverStyle }}
+                        style={bookHoverStyle}
                       />
-                      <Fade
-                        in={hover[slug]}
-                        {...(hover ? { timeout: 400 } : {})}
-                      >
+                      <Overlay>
                         <div
                           style={{
-                            width: '100%',
-                            height: '100%',
-                            position: 'absolute',
-                            top: 0,
-                            backdropFilter: 'blur(3px)',
-                            backgroundColor: '#52525294',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            textAlign: 'center',
+                            width: '75%',
+                            margin: '0 auto',
                           }}
                         >
-                          <div
-                            style={{
-                              width: '75%',
-                              margin: '0 auto',
+                          <Typography
+                            sx={{
+                              fontWeight: '600',
+                              fontSize: '20px',
+                              color: 'primary.light',
                             }}
                           >
-                            <Typography
-                              sx={{
-                                fontWeight: '600',
-                                fontSize: '20px',
-                                color: 'primary.light',
-                              }}
-                            >
-                              {item.titulo}
-                            </Typography>
-                            <Typography
-                              sx={{
-                                fontWeight: '500',
-                                fontSize: '15px',
-                                color: 'white',
-                              }}
-                            >
-                              {item.editora.titulo} · {item.ano.ano}
-                            </Typography>
-                          </div>
+                            {item.titulo}
+                          </Typography>
+                          <Typography
+                            sx={{
+                              fontWeight: '500',
+                              fontSize: '15px',
+                              color: 'white',
+                            }}
+                          >
+                            {item.editora.titulo} · {item.ano.ano}
+                          </Typography>
                         </div>
-                      </Fade>
-                    </div>
+                      </Overlay>
+                    </ImageContainer>
                   </Link>
                 </Box>
               )
             })}
           </Slider>
-        </Box>
-      </Container>
+        </StyledBox>
+      </StyledContainer>
     </>
   )
 }
@@ -197,8 +177,4 @@ interface Props {
   titulo: string
   itens: LivroProps[]
   parentRoute?: string
-}
-
-interface HoverProps {
-  [index: string]: boolean
 }
