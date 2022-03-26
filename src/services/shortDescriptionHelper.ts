d4e01b6c46880baaec25d06210878dc000da8d24
Guilherme Almeida <guica.almeida@gmail.com>
2022-03-26 18:43:37 -0300

    fix: improved short desc helper

--- src/services/shortDescriptionHelper.ts
+++ src/services/shortDescriptionHelper.ts
@@ -2,5 +2,14 @@ import { stripHtml } from 'string-strip-html'
 
 export const getShortDescription = (string = '', charcount = 240) => {
   const strippedTags = stripHtml(string)
-  return string ? `${strippedTags.result.slice(0, charcount)}...` : ''
+  const sliced = strippedTags.result.slice(0, charcount)
+  if (sliced.length === strippedTags.result.length) {
+    return strippedTags.result
+  } else if (sliced.charAt(sliced.length - 1) === ' ') {
+    return `${sliced.slice(0, sliced.length - 2)}...`
+  } else if (sliced.charAt(sliced.length - 1) === '.') {
+    return `${sliced.slice(0, sliced.length - 2)}...`
+  } else {
+    return `${sliced}...`
+  }
 }
