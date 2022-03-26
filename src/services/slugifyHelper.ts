e07604053662136a88250963e584f9d870285b91
Guilherme Almeida <guica.almeida@gmail.com>
2022-03-26 16:11:04 -0300

    feat: tooling improved

--- src/services/slugifyHelper.ts
+++ src/services/slugifyHelper.ts
@@ -1,6 +1,6 @@
 import slugify from 'slugify'
 
-export default function universalSlugify(string: string) {
+export default function universalSlugify(string = '') {
   return slugify(string, {
     lower: true,
     remove: /[.,:/"()]/g,
