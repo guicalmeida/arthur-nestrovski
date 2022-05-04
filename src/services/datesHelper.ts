4225a78dcf9c372b376896f2b5cd5f21a67fd032
Guilherme Almeida <guica.almeida@gmail.com>
2022-05-03 23:24:26 -0300

    feat: agenda page almost finished

--- src/services/datesHelper.ts
+++ src/services/datesHelper.ts
@@ -19,3 +19,23 @@ export const formatIsoString = (date: string, format?: string) => {
 export const isDate1AfterDate2 = (date1: string, date2: string) => {
   return dayjs(date1).isAfter(dayjs(date2))
 }
+
+export const getEventDateInfo = (date1: string, date2: string): string => {
+  if (date2) {
+    if (dayjs(date1).isSame(dayjs(date2), 'day')) {
+      return `${dayjs(date1).format('DD/MM/YYYY')}, das ${dayjs(date1).format(
+        'HH:mm'
+      )} às ${dayjs(date2).format('HH:mm')}`
+    } else {
+      return `De ${dayjs(date1).format('DD/MM/YYYY')} às ${dayjs(date1).format(
+        'HH:mm'
+      )} a ${dayjs(date2).format('DD/MM/YYYY')} às ${dayjs(date2).format(
+        'HH:mm'
+      )}`
+    }
+  } else {
+    return `${dayjs(date1).format('DD/MM/YYYY')}, às ${dayjs(date1).format(
+      'HH:mm'
+    )}`
+  }
+}
--- src/templates/agenda/index.tsx
+++ src/templates/agenda/index.tsx
@@ -1,9 +1,185 @@
-import { EventosProps } from 'types/api'
+import NavDrawer from 'components/navDrawer'
+import NavHeader from 'components/navHeader'
+import TextsCard from 'components/textsCard'
+import dayjs from 'dayjs'
+import { getShortDescription } from 'services/shortDescriptionHelper'
+import universalSlugify from 'services/slugifyHelper'
+import { EventoProps, EventosProps } from 'types/api'
+import 'dayjs/locale/pt-br'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+import { Container, Typography } from '@mui/material'
+import { getEventDateInfo } from 'services/datesHelper'
+
+dayjs.locale('pt-br')
+dayjs.extend(customParseFormat)
 
 export default function AgendaPage({ eventos }: EventosProps) {
+  const bc = {
+    Agenda: 'agenda',
+  }
+
+  console.log(eventos)
+
+  const months: { [key: string]: EventoProps[] } = {}
+
+  eventos.forEach((evento) => {
+    const dateIndex = dayjs(evento.inicio).format('MMMM YYYY')
+
+    if (!Object.keys(months).includes(dateIndex)) {
+      months[dateIndex] = []
+    }
+    months[dateIndex].push(evento)
+  })
+
+  const thisMonthEvents = eventos.filter((evento) =>
+    dayjs().isSame(dayjs(evento.inicio), 'month')
+  )
+  const hasEventsThisMonth = thisMonthEvents && thisMonthEvents.length > 0
+
   return (
     <>
-      <div></div>
+      <NavDrawer />
+      <NavHeader breadcrumbs={bc}>AGENDA</NavHeader>
+      <Container>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '24px',
+            margin: '24px',
+          }}
+        >
+          {hasEventsThisMonth && (
+            <>
+              <Typography
+                sx={{
+                  fontWeight: 600,
+                  letterSpacing: '5px',
+                  marginTop: '10px',
+                  textAlign: 'middle',
+                }}
+                variant="h4"
+                component="h2"
+                color="text.secondary"
+              >
+                ESTE MÊS
+              </Typography>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  gap: '24px',
+                  margin: '24px',
+                }}
+              >
+                {thisMonthEvents.map((evento) => {
+                  const shortText = getShortDescription(evento?.descricao?.html)
+                  const slug = universalSlugify(evento?.titulo)
+                  const { inicio, fim } = evento || {}
+
+                  const date = getEventDateInfo(inicio, fim)
+
+                  return (
+                    <TextsCard
+                      key={slug}
+                      text={{
+                        title: evento?.titulo,
+                        date,
+                        imageUrl: evento?.capa?.url,
+                      }}
+                      path={`/agenda/${slug}`}
+                      calendar={true}
+                    >
+                      {shortText}
+                    </TextsCard>
+                  )
+                })}
+              </div>
+              <div
+                role="separator"
+                style={{
+                  height: '1px',
+                  width: '100%',
+                  backgroundColor: 'lightgrey',
+                }}
+              ></div>
+              <Typography
+                sx={{
+                  fontWeight: 600,
+                  letterSpacing: '5px',
+                  marginTop: '10px',
+                  textAlign: 'middle',
+                }}
+                variant="h4"
+                component="h2"
+                color="text.secondary"
+              >
+                TODOS EVENTOS
+              </Typography>
+            </>
+          )}
+          {Object.entries(months)
+            .sort((a, b) => {
+              return dayjs(a[0], 'MMMM YYYY').isAfter(dayjs(b[0], 'MMMM YYYY'))
+                ? -1
+                : 1
+            })
+            .map((month) => {
+              return (
+                <div key={month[0]}>
+                  <Typography
+                    sx={{
+                      fontWeight: 400,
+                      letterSpacing: '5px',
+                      marginTop: '10px',
+                    }}
+                    variant="h4"
+                    component="h2"
+                    color="text.secondary"
+                  >
+                    {month[0].toUpperCase()}
+                  </Typography>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      gap: '24px',
+                      margin: '24px',
+                    }}
+                  >
+                    {month[1].map((evento) => {
+                      const shortText = getShortDescription(
+                        evento?.descricao?.html
+                      )
+                      const slug = universalSlugify(evento?.titulo)
+                      const { inicio, fim } = evento || {}
+
+                      const date = getEventDateInfo(inicio, fim)
+
+                      return (
+                        <TextsCard
+                          key={slug}
+                          text={{
+                            title: evento?.titulo,
+                            date,
+                            imageUrl: evento?.capa?.url,
+                          }}
+                          path={`/agenda/${slug}`}
+                          calendar={true}
+                        >
+                          {shortText}
+                        </TextsCard>
+                      )
+                    })}
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+      </Container>
     </>
   )
 }
