1ca2ff816ce9091a5a36d642bf78409a90fd87b6
Guilherme Almeida <guica.almeida@gmail.com>
2022-05-28 14:30:23 -0300

    feat: contato created

---
+++ src/pages/contato.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import ContatoPage from 'templates/contato'
+
+export default function Contato() {
+  return (
+    <>
+      <Head>
+        <title>Contato · Arthur Nestrovski</title>
+      </Head>
+      <ContatoPage />
+    </>
+  )
+}
---
+++ src/templates/contato.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import { Box, Button, Container, TextField } from '@mui/material'
+import NavDrawer from 'components/navDrawer'
+import NavHeader from 'components/navHeader'
+
+export default function ContatoPage() {
+  const bc = {
+    Contato: 'contato',
+  }
+  return (
+    <>
+      <NavDrawer />
+      <NavHeader breadcrumbs={bc}>CONTATO</NavHeader>
+      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+        <Box sx={{ width: '100%', maxWidth: '600px' }}>
+          <form
+            style={{ display: 'flex', flexDirection: 'column' }}
+            name="contato"
+            method="post"
+            data-netlify="true"
+          >
+            <input type="hidden" name="contato" value="contato" />
+            <TextField required label="Nome" variant="filled" />
+            <TextField required label="E-mail" variant="filled" />
+            <TextField required label="Assunto" variant="filled" />
+            <TextField
+              required
+              label="Mensagem"
+              variant="filled"
+              multiline
+              minRows={6}
+            />
+            <Button variant="contained" type="submit">
+              Enviar
+            </Button>
+          </form>
+        </Box>
+      </Container>
+    </>
+  )
+}
