ed89ccda19455e855a915d89cd59bc3150b4ed53
Guilherme Almeida <guica.almeida@gmail.com>
2022-05-28 16:04:34 -0300

    feat: add both images

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,27 +1,34 @@
 import { Paper, Container, Typography, Grid } from '@mui/material'
-
-const homeStyle = {
-  paper: {
-    height: '100vh',
-    backgroundImage:
-      'url(https://www.arthurnestrovski.com.br/wp-content/uploads/2021/02/arthur-3-lq.jpg)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'right',
-  },
-  container: {
-    height: '100%',
-  },
-  content: {
-    height: '100%',
-  },
-  heroTitle: {
-    textShadow: '2px 4px 3px rgba(0,0,0,0.3)',
-    color: 'primary.main',
-    textTransform: 'uppercase',
-  },
-}
+import { useState } from 'react'
 
 const HeroSection = () => {
+  const [image, setImage] = useState(
+    'https://media.graphassets.com/z8hK7XzHTFulOXlm9ERw'
+  )
+
+  //TODO: intercalar imagens
+  const img1 = 'https://media.graphassets.com/z8hK7XzHTFulOXlm9ERw'
+  const img2 = 'https://media.graphassets.com/I60zy8XYTMmxbBrDncQ1'
+
+  const homeStyle = {
+    paper: {
+      height: '100vh',
+      backgroundImage: `url(${image})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'right',
+    },
+    container: {
+      height: '100%',
+    },
+    content: {
+      height: '100%',
+    },
+    heroTitle: {
+      textShadow: '2px 4px 3px rgba(0,0,0,0.3)',
+      color: 'primary.main',
+      textTransform: 'uppercase',
+    },
+  }
   return (
     <Paper sx={homeStyle.paper}>
       <Container sx={homeStyle.container}>
