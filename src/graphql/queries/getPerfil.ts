7427aa271096144542d41c41ecf6a92c5369cd8e
Guilherme Almeida <guica.almeida@gmail.com>
2022-06-01 21:19:30 -0300

    fix: mudar CV

--- src/graphql/queries/getPerfil.ts
+++ src/graphql/queries/getPerfil.ts
@@ -3,7 +3,7 @@ const GET_PERFIL = /* GraphQL */ `
     foto: asset(where: { id: "cl14ae9x09kzm0bkfwet1pn9n" }) {
       url
     }
-    cv: asset(where: { id: "cl14ae9oja19g0dlppbx3i27l" }) {
+    cv: asset(where: { id: "cl3w9phpbzg9x0elrkmqxk1iv" }) {
       url
     }
   }
