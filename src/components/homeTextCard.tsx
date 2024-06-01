7beff1cc7a0273b23b4cb7f4353b8aacf8114fa1
Guilherme Almeida <johndoe@example.com>
2024-06-01 11:19:55 -0300

    fix: card at home

--- src/components/homeTextCard.tsx
+++ src/components/homeTextCard.tsx
@@ -18,7 +18,7 @@ const StyledCard = styled(Card)`
   @media (min-width: 1200px) {
     display: flex;
     min-height: 350px;
-    max-width: 740px;
+    min-width: 100%;
   }
 `
 const StyledBox = styled(Box)`
