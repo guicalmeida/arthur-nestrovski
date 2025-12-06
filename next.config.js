32e39a96067e5108915365c24c666589454416b5
Guilherme de Almeida <eu@ogui.ca>
2025-12-06 10:18:29 -0300

    fix build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.graphcms.com',
-      },
-    ],
-  },
-  staticPageGenerationTimeout: 10000,
-  experimental: {
-    cpus: 1,
-    workerThreads: false,
-  },
-}
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "media.graphcms.com",
+			},
+		],
+	},
+	staticPageGenerationTimeout: 10000,
+};
